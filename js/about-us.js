b751a971ab09fc2d4ce4c35d692d488b50c695d2
Romel Campos <rcoolah@gmail.com>
2016-09-26 15:44:01 +0800

    Fetch favourites from CMS

--- js/about-us.js
+++ js/about-us.js
@@ -1,14 +1,61 @@
-$(document).ready(function() {
+var Favourite = function(opts) {
+  var scope = this;
+  var url = window.location.hostname == 'www.bpl-digital.com' ? 'https://new.barpass.co.uk' : 'https://staging.barpass.co.uk';
+
+  scope.el = $('#favourite-items');
+
+  scope.init = function() {
+    if (scope.el.length == 0) return;
+    scope.fetchFavourites(scope.onFavouritesFetched);
+  };
+
+  scope.fetchFavourites = function(cb) {
+    $.get(url + '/api/website/favourites.json', cb).fail(scope.onFailedRequest);
+  };
+
+  scope.onFavouritesFetched = function(data) {
+    scope.el.empty();
+    if (data && data.length > 0) {
+      $.each(data, function(_, favourite) {
+        scope.renderFavourite(favourite);
+      });
+    }
+  };
+
+  scope.renderFavourite = function(favourite) {
+    scope.loadTmpl('favourite-item-template', favourite, function(html) {
+      scope.el.fadeOut(function() {
+        scope.el.append(html).fadeIn('slow');
+
+      });
+    });
+  };
+
+  this.loadTmpl = function(tmplId, context, cb) {
+    if (typeof(context) === 'undefined') context = {};
+    var source   = $("#" + tmplId).html();
+    var template = Handlebars.compile(source);
+    var html = template(context);
+    cb(html);
+  };
+
+  scope.onFailedRequest = function(xhr) {
+    console.log(xhr.responseText);
+    scope.onFavouritesFetched({items: []});
+  };
+};
+
+Handlebars.registerHelper('hideUrlProtocol', function(url) {
+  return url.replace(/.*?:\/\//g, '');
+});
+
+var onReady = function() {
   if (window.innerHeight < 530) {
     $('.cover .page-title').css('height', '150px');
   }
+  var favourite = new Favourite();
+  favourite.init();
+};
 
-  var favouriteSection = $('section#favourite');
-  if (favouriteSection.length) {
-      var apiUrl = window.location.hostname == 'www.bpl-digital.com' ? 'https://new.barpass.co.uk' : 'http://localhost:3000';
-      $.get(apiUrl + '/api/website/favourites.json', function(data) {
-        console.log(data);
-      });
-  }
-});
-
+$(document).ready(onReady);
+$(document).on('page:load', onReady);
