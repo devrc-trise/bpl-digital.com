e5705033f02f33cb1b0c7501ce79cc158bbbf0cc
Romel Campos <rcoolah@gmail.com>
2016-09-27 12:48:37 +0800

    Add hotjar

--- js/scripts.js
+++ js/scripts.js
@@ -1219,3 +1219,13 @@ $(document).ready(function() {
         $('.submenu-bar ul li a.' + $(top.pop()).attr('id')).addClass('active');
     });
 });
+
+// Hotjar Tracking Code for http://www.bpl-digital.com/
+(function(h,o,t,j,a,r){
+    h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+    h._hjSettings={hjid:296407,hjsv:5};
+    a=o.getElementsByTagName('head')[0];
+    r=o.createElement('script');r.async=1;
+    r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+    a.appendChild(r);
+})(window,document,'//static.hotjar.com/c/hotjar-','.js?sv=');
